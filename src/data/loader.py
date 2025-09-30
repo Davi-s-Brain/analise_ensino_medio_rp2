a9e7c09e3c24f57a6a03ff2dae56d6796007ca58
Davi B. <davidedeus2010@gmail.com>
2025-09-29 23:04:31 -0300

    Adicinoando outros micro dados e agregaćão

--- src/data/loader.py
+++ src/data/loader.py
@@ -127,56 +127,67 @@ class DataLoader:
             'IN_PROF_ASSIST_SOCIAL',
             'IN_EXAME_SELECAO',
             'IN_ORGAO_GREMIO_ESTUDANTIL',
-            'QT_DOC_MED',
-            'IN_FINAL_SEMANA', # Nâo pegou
-            'QT_SALAS_EXISTENTES', # Nâo pegou
-            'QT_FUNCIONARIOS', # Nâo pegou
+            'QT_DOC_MED', # Média, desvio padrão, min e max
+            'IN_FINAL_SEMANA',
+            'QT_SALAS_EXISTENTES', # Média, desvio padrão, min e max
+            'QT_FUNCIONARIOS', # Média, desvio padrão, min e max
             'QT_MAT_MED',
             'QT_MAT_MED_INT'
         ]
         
         dicionario_microdados = pd.read_excel('data/dicionario_dados_educacao_basica.xlsx', skiprows=6)
         
-        with ZipFile('data/microdados_ed_basica_2024.zip') as z: #BAIXAR OS DADOS DE 2020
-            with z.open('microdados_ed_basica_2024.csv') as f:
+        with ZipFile('data/microdados_censo_escolar_2020.zip') as z:
+            with z.open('microdados_ed_basica_2020/dados/microdados_ed_basica_2020.CSV') as f:
                 sample = pd.read_csv(f, sep=';', encoding='latin1', nrows=0)
                 available_columns = sample.columns.tolist()
 
         campos_relevantes_evasao = (dicionario_microdados[
             dicionario_microdados['Nome da Variável'].isin(campos_interesse)]
             .iloc[:, 1].tolist())
-        
-
-        print(campos_relevantes_evasao)
-        print('---'*30)
-        print(available_columns)
-
 
         campos_existentes = [col for col in campos_relevantes_evasao if col in available_columns]
 
-        with ZipFile('data/microdados_ed_basica_2024.zip') as z:
-            with z.open('microdados_ed_basica_2024.csv') as f:
-                microdados = pd.read_csv(f, sep=';', encoding='latin1', 
-                                       usecols=campos_existentes)
+        with ZipFile('data/microdados_censo_escolar_2020.zip') as z:
+            with z.open('microdados_ed_basica_2020/dados/microdados_ed_basica_2020.CSV') as f:
+                microdados = pd.read_csv(f, sep=';', encoding='latin1', usecols=campos_existentes)
                 microdados = microdados.fillna(microdados.mean(numeric_only=True))
     
         microdados['NO_UF'] = microdados['NO_UF'].map(estados_br)
-
-        print('---'*30)
-        print(microdados.columns.to_list())
-        print('---'*30)
-
     
         df_municipio = (microdados[microdados['TP_DEPENDENCIA'] != 4]
                        .drop(columns=['TP_DEPENDENCIA'])
                        .groupby(['NO_UF', 'NO_MUNICIPIO'])
-                       .agg("mean")
+                       .agg({
+                            'QT_DOC_MED': ['mean', 'std', 'min', 'max'],
+                            'QT_SALAS_EXISTENTES': ['mean', 'std', 'min', 'max'],
+                            'QT_FUNCIONARIOS': ['mean', 'std', 'min', 'max'],
+                            'IN_PREDIO_COMPARTILHADO': 'mean',
+                            'IN_AGUA_INEXISTENTE': 'mean',
+                            'IN_ENERGIA_INEXISTENTE': 'mean',
+                            'IN_ESGOTO_INEXISTENTE': 'mean',
+                            'IN_BIBLIOTECA': 'mean',
+                            'IN_LABORATORIO_INFORMATICA': 'mean',
+                            'IN_QUADRA_ESPORTES': 'mean',
+                            'IN_REFEITORIO': 'mean',
+                            'IN_INTERNET': 'mean',
+                            'IN_INTERNET_ALUNOS': 'mean',
+                            'IN_BANDA_LARGA': 'mean',
+                            'IN_PROF_PSICOLOGO': 'mean',
+                            'IN_PROF_ASSIST_SOCIAL': 'mean',
+                            'IN_EXAME_SELECAO': 'mean',
+                            'IN_ORGAO_GREMIO_ESTUDANTIL': 'mean',
+                            'IN_FINAL_SEMANA': 'mean',
+                            'QT_MAT_MED': 'sum',
+                            'QT_MAT_MED_INT': 'sum'
+                        })
                        .reset_index())
-    
-        print(df_municipio.columns.to_list())
-        print(len(df_municipio.columns))
-        print('---'*30)
-        print(df_municipio.head())
+
+        df_municipio.columns = [
+            '_'.join(col).strip() if isinstance(col, tuple) else col 
+            for col in df_municipio.columns.values
+        ]
+
         return df_municipio
     
     def combine_data(self, df_inep, df_inse):
